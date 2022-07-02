1b19e8c860d240d3fc19fab400f22a5e6ac2bf23
Andrey <velociraptor@live.ru>
2022-07-02 16:06:48 +0300

    game relese

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert ('Hello');
+const player = document.getElementById('player');
+const wall = document.getElementById('wall');
+
+document.addEventListener('keydown', function(event) {
+    jump();
+})
+
+function jump() {
+   if (player.classList != 'jump')
+   {
+       player.classList.add('jump');
+   }
+   setTimeout(function (){
+       player.classList.remove('jump')
+   }, 1100)
+}
+
+let isAlive = setInterval(function() {
+    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
+    let wallLeft = parseInt(window.getComputedStyle(wall).getPropertyValue('left'))
+
+    if (wallLeft < 50 && wallLeft > 0 && playerTop >= 140) {
+        alert('game over');
+    }
+}, 10)
